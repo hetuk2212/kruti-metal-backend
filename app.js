c695090a0bc39b08559892eab7c9fa9462b324c4
Hetuk Patel <84185863+hetukpatel2212@users.noreply.github.com>
2025-02-01 20:31:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,23 +6,17 @@ const { port } = require('./config/config');
 
 const app = express();
 
-// Enable CORS for specific origins (local and live)
-app.use(cors({
-  origin: ['http://localhost:3000', 'https://kruti-metal-backend.onrender.com'], // Add your live domain here
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-}));
-
 // Middleware
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-
-// Routes
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
   res.json({ message: 'Fetching all users' });
 });
+
 app.use('/api/users', userRoutes);
 
-// Error handling middleware (optional)
+
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send({ error: 'Something went wrong!' });
